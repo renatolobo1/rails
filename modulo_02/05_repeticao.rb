15692eeb48a4b7d278d16833e0f2cfbc33a80aa9
Renato <renatolobo_@hotmail.com>
2020-02-07 09:47:37 -0300

    Aula Repetiçao

---
+++ modulo_02/05_repeticao.rb
@@ -0,0 +1,18 @@
+
+# Each
+
+(0..5).each do |i|
+    puts "O valor lido foi: " + i.to_s
+end
+
+
+
+# While
+
+# i = 0
+# num = 5
+
+# while i < num do
+#     puts "Contando..." + i.to_s
+#     i += 1
+# end
