fabcf995e0f1a417e70eb34b484dce5259e3b16a
Renato <renatolobo_@hotmail.com>
2020-02-07 10:29:49 -0300

    Aula array

---
+++ modulo_02/06_array.rb
@@ -0,0 +1,32 @@
+v = [[1,3,5],[0,2,4],[7,9,6]]
+
+v.each do |externo|
+    externo.each do |interno|
+        puts interno
+    end
+end
+
+
+
+
+# s = "Renato"
+# puts s[1..]
+
+
+# v = [1,3,5,6,7,9]
+# v.push("OI")
+# v.each do |item|
+#     puts item
+# end
+
+# v1 = Array.new
+# v1.push(4)
+# v1.push("OI")
+# v1.push('afff')
+# v1.push('amem')
+
+# puts v1[3]
+
+# v1.each do |elem|
+#     puts elem
+# end
