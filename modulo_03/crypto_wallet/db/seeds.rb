08046700fd92caa9bd3a2c329fced5642fd5e768
Renato <renatolobo_@hotmail.com>
2020-02-19 16:27:38 -0300

    Aula db:seed

--- modulo_03/crypto_wallet/db/seeds.rb
+++ modulo_03/crypto_wallet/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cadastrando as moedas...."
+
+Coin.create!(
+    description: "Bitcoin",
+    acronym: "BTC",
+    url_imagem: "http://pngimg.com/uploads/bitcoin/bitcoin_PNG47.png"
+)
+
+Coin.create!(
+    description: "Dollar",
+    acronym: "$",
+    url_imagem: "https://img2.gratispng.com/20171128/f9e/gold-coin-transparent-png-clipart-5a1d22de435229.9520313415118589102758.jpg"
+)
+
+Coin.create!(
+    description: "Dash",
+    acronym: "DA",
+    url_imagem: "https://img2.gratispng.com/20171128/f9e/gold-coin-transparent-png-clipart-5a1d22de435229.9520313415118589102758.jpg"
+)
+
+puts "Moedas cadastrada com sucesso...."
