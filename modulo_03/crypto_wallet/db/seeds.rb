2fca05e9416ff2e76be65184fbdf18cef7b1aeca
Renato Lobo <renatolobo_@hotmail.com>
2020-02-25 17:56:10 -0300

    Aula Find_or_create_by

--- modulo_03/crypto_wallet/db/seeds.rb
+++ modulo_03/crypto_wallet/db/seeds.rb
@@ -6,24 +6,37 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Cadastrando as moedas...."
+spinner = TTY::Spinner.new("[:spinner] Cadastrando Moedas", format: :dots_2)
+      spinner.auto_spin
 
-Coin.create!(
+Coin.find_or_create_by!(
     description: "Bitcoin",
     acronym: "BTC",
     url_imagem: "http://pngimg.com/uploads/bitcoin/bitcoin_PNG47.png"
 )
 
-Coin.create!(
-    description: "Dollar",
-    acronym: "$",
-    url_imagem: "https://img2.gratispng.com/20171128/f9e/gold-coin-transparent-png-clipart-5a1d22de435229.9520313415118589102758.jpg"
-)
 
-Coin.create!(
-    description: "Dash",
-    acronym: "DA",
-    url_imagem: "https://img2.gratispng.com/20171128/f9e/gold-coin-transparent-png-clipart-5a1d22de435229.9520313415118589102758.jpg"
-)
+coins = [
+    
+            {
+                description: "Bitcoin",
+                acronym: "BTC",
+                url_imagem: "http://pngimg.com/uploads/bitcoin/bitcoin_PNG47.png"
+            },
+            {
+                description: "Dollar",
+                acronym: "$",
+                url_imagem: "https://img2.gratispng.com/20171128/f9e/gold-coin-transparent-png-clipart-5a1d22de435229.9520313415118589102758.jpg"
+            },
+            {
+                description: "Dash",
+                acronym: "DA",
+                url_imagem: "https://img2.gratispng.com/20171128/f9e/gold-coin-transparent-png-clipart-5a1d22de435229.9520313415118589102758.jpg"
+            }
+        ]
 
-puts "Moedas cadastrada com sucesso...."
+coins.each do |coin|
+    Coin.find_or_create_by!(coin)
+end
+
+spinner.success("Moedas cadastrada com sucesso....") 
