92d399b1feb5a81e7895238d25a631b63f5f31c7
Renato Lobo <renatolobo_@hotmail.com>
2020-03-15 15:46:15 -0300

    Aula usando o I18N

--- modulo_03/crypto_wallet/app/helpers/application_helper.rb
+++ modulo_03/crypto_wallet/app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+    def locale(locale)
+        if I18n.locale == :en
+            "Estados Unidos"
+        else
+            "Português do Brasil"
+        end
+    end
+    
     def data_br(data_us)
         data_us.strftime("%d/%m/%Y")
     end
