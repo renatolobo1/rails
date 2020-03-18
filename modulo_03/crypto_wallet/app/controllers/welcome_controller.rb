613935c9f356db762a4417743d6e38dac7aac1cb
Renato <renatolobo_@hotmail.com>
2020-03-18 15:45:39 -0300

    Aula cookies e session

--- modulo_03/crypto_wallet/app/controllers/welcome_controller.rb
+++ modulo_03/crypto_wallet/app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    session[:user_name]= "Renato Lôbo [SESSION]"
+    cookies[:curso]= "Curso de Ruby on Rails - Jackson Pires [COOKIE]"
     @meu_nome = params[:nome]
     @curso = "Rails"
   end
