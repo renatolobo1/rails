72ac3e9db06a19e46f994c848cea6521c5845631
Renato Lobo <renatolobo_@hotmail.com>
2020-03-20 13:27:46 -0300

    Aula usando uma biblioteca JS VENDOR

--- modulo_03/crypto_wallet/config/initializers/assets.rb
+++ modulo_03/crypto_wallet/config/initializers/assets.rb
@@ -18,6 +18,8 @@ Rails.application.config.assets.precompile += %w( coins.css
                                                 )
 Rails.application.config.assets.precompile += %w( cable.js
                                                   coins.js
+                                                  jquery.js
                                                   mining_types.js
+                                                  notify.js
                                                   welcome.js        
                                                 )
