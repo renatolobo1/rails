f1bcb200211b253c4e8bb6e5dbf07b4a297c44d6
Renato Lobo <renatolobo_@hotmail.com>
2020-03-28 14:30:40 -0300

    Aula corrigindo o sistema

--- modulo_05/time_to_answer/config/routes.rb
+++ modulo_05/time_to_answer/config/routes.rb
@@ -2,17 +2,17 @@ Rails.application.routes.draw do
   namespace :site do
     get 'welcome/index'
   end
-  namespace :profiles_backoffice do
+  namespace :users_backoffice do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
     get 'welcome/index'
   end
-  devise_for :profiles
   devise_for :admins
-  
+  devise_for :users
 
   get 'inicio', to: 'site/welcome#index'
+  
   root to: 'site/welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
